8b997c7679c3cd52f046120b8e1f638eaecc2a4c
Andy Yin <ayin@cloudfloor.com>
2011-04-12 16:13:20 +0800

    ignore connection reset

--- lib/whois/server/adapters/verisign.rb
+++ lib/whois/server/adapters/verisign.rb
@@ -38,22 +38,26 @@ module Whois
           buffer_append response, host
 
           if endpoint = extract_referral(response)
-            response = query_the_socket(string, endpoint, DEFAULT_WHOIS_PORT)
-            buffer_append response, endpoint
+            begin
+              response = query_the_socket(string, endpoint, DEFAULT_WHOIS_PORT)
+              buffer_append response, endpoint
+            rescue ConnectionError => error
+              #do nothing, some whois servers will reset this query, just ignore it.
+            end
           end
         end
 
 
         private
 
-          def extract_referral(response)
-            if response =~ /Domain Name:/
-              endpoint = response.scan(/Whois Server: (.+?)$/).flatten.last
-              endpoint.strip!
-              endpoint = nil  if endpoint == "not defined"
-              endpoint
-            end
+        def extract_referral(response)
+          if response =~ /Domain Name:/
+            endpoint = response.scan(/Whois Server: (.+?)$/).flatten.last
+            endpoint.strip!
+            endpoint = nil  if endpoint == "not defined"
+            endpoint
           end
+        end
 
       end
 
